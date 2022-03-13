dcc7ccbb4d2824d364d791fa4c05c91c706951b7
laiconglin <laiconglin@douyu.tv>
2022-03-14 00:38:40 +0800

    update docker php compose

--- DockerfilePHP7.4Fpm/build.sh
+++ DockerfilePHP7.4Fpm/build.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-docker build --no-cache -t php:7.1.latest-fpm .
+docker build --no-cache -t php:7.4.latest-fpm .
 docker container prune --force
 docker image prune --force
--- rebuildall.sh
+++ rebuildall.sh
@@ -1,12 +1,7 @@
 #! /bin/bash
-cd ./DockerfileMysql5.7/
-bash ./build.sh
-cd ../DockerfileNginx1.13.8/
-bash ./build.sh
-cd ../DockerfilePHP5.6Fpm/
-bash ./build.sh
-cd ../DockerfilePHP7.1Fpm/
-bash ./build.sh
+docker build --no-cache -f ./DockerfileMysql5.7/Dockerfile -t mysql:5.7.22.latest .
+docker build --no-cache -f ./DockerfileNginx1.21.6/Dockerfile -t nginx:1.21.6.latest .
+docker build --no-cache -f ./DockerfilePHP7.4Fpm/Dockerfile -t php:7.4.latest-fpm .
 # delete not used images
 docker container prune --force
 docker image prune --force
